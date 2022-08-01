1f32099fecbeacfe575ee9b79478783ae23fa30a
Mahesh777600 <110294330+Mahesh777600@users.noreply.github.com>
2022-08-01 21:27:53 +0530

    By Mahesh

--- Git01/src/com/GitRepsitory.java
+++ Git01/src/com/GitRepsitory.java
@@ -6,6 +6,10 @@ public class GitRepsitory {
 	{
 	  System.out.println("This is M1");
 	}
+	public void m2()
+	{
+	  System.out.println("This is M2");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("main");
