da2ce64c22a03747347d8a437ce721c0446426fb
Mahesh777600 <110294330+Mahesh777600@users.noreply.github.com>
2022-08-01 21:24:02 +0530

    Update GitRepsitory.java

--- Git01/src/com/GitRepsitory.java
+++ Git01/src/com/GitRepsitory.java
@@ -1,6 +1,11 @@
 package com;
 
 public class GitRepsitory {
+	
+	public void m1()
+	{
+	  System.out.println("This is M1");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("main");
